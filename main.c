b64d09f26b08501132cf048166f2d15665f5fc29
othmanekahtal <othmanekahtal@gmail.com>
2021-01-10 21:08:44 +0100

    fix issue#1:Arrays

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 
 void main() {
     int Num_P;
-    char Name[50];
     printf("welcome to Presidential election\n");
     printf("=>Enter the number of presidents nominated for the presidential elections: ");
     scanf("%d",&Num_P);
-    for (int i = 1; i <=Num_P ; ++i) {
-        printf("Enter The Name of The Presidents %d :",i);
+    int Table[Num_P];
+    for (int i = 0; i <Num_P ; i++) {
+        printf("Enter The Name of The Presidents %d :",i+1);
+        scanf(" %d", &Table[i]);
         printf("\n");
     }
 }
