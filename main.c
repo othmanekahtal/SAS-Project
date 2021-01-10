d7571a844b79732979b62477f9039ecbd4fb9488
othmanekahtal <othmanekahtal@gmail.com>
2021-01-10 20:26:04 +0100

    add loop in project

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+void main() {
+    int Num_P;
+    char Name[50];
+    printf("welcome to Presidential election\n");
+    printf("=>Enter the number of presidents nominated for the presidential elections: ");
+    scanf("%d",&Num_P);
+    for (int i = 1; i <=Num_P ; ++i) {
+        printf("Enter The Name of The Presidents %d :",i);
+        printf("\n");
+    }
 }
