cb42e79deeb6f2c6d470f7510ea23d36f09342f0
othmanekahtal <othmanekahtal@gmail.com>
2021-01-12 00:28:09 +0100

    add more to project SAS:count the Vote to President

--- main.c
+++ main.c
@@ -5,23 +5,19 @@ typedef struct
     char name[50];
     int votes;
 }President;
-President addNamePresident(int i,President president){
-    printf("==>Enter the Name of the President %d : ",i+1);
-    /*
-        scanf("%s",president.name);
-        you can't use the scanf in this case scanf not read whitespaces;
-    */
-    scanf(" %[^\n]s", president.name);
-    printf("%s\n",president.name);
-    return president;
+void addNamePresident(President president[],int Num__p){
+    for (int i = 0;i<Num__p;i++){
+        printf("==>Enter the Name of the President %d : ",i+1);
+        scanf(" %[^\n]s", president[i].name);
+        president[i].votes = 0;
+    }
 }
 typedef struct{
     char CIN[10];
 }electors;
-electors voting(int i,electors CIN){
+void voting(int i,electors CIN){
     printf("==>You're Elector %d, Please Enter your CNI : ",i+1);
     scanf("%s",CIN.CIN);
-    return CIN;
 }
 void main(){
     int Num_P;
@@ -31,26 +27,24 @@ void main(){
     printf("=>Enter the number of presidents nominated for the presidential elections (minimum 5) : ");
     scanf("%d",&Num_P);
     President P[Num_P];
-    P[Num_P].votes = 0;
-    for (int i = 0; i < Num_P; i++){
-        addNamePresident(i,P[i]);
-    }
+    addNamePresident(P,Num_P);
     system("clear");
     printf("=>Enter The Number of electors :");
     scanf("%d",&Num_E);
     electors E[Num_E];
     for (int j = 0;j<Num_E; j++){
         voting(j, E[j]);
-        system("clear");
-        printf("==>Choice one of The Presidents : ");
+        printf("==>Choice one of The Presidents : \n");
         for (int k = 0;k<Num_P;k++){
-            printf(" %d.%s",k+1,P[k].name);
+            printf(" %d.%s\t",k+1,P[k].name);
         }
+        printf(":");
         scanf("%d", &choice);
-            for (int l = 0;l<Num_P;l++){
-                if(choice==l){
-                    P[l].votes += 1;
-                }
-            }
+        P[choice-1].votes += 1;
+        printf("P[%d].votes = %d\n",choice,P[choice-1].votes);
+    }
+    printf("Final Result:\n");
+    for(int l =0;l<Num_P;l++){
+        printf("%s\t =\t %d\n", P[l].name,P[l].votes);
     }
 };
